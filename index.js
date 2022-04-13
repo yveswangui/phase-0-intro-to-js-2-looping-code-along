0367a9af968e5a282c125c4cdedcaaeed0753564
Yvonne <yveswangui@gmail.com>
2022-04-13 19:53:38 +0300

    commit tree 2457c737d855ea1dbf14a393ccad206d8be92fc3

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+
+function writeCards(names,randomName){
+    for(i = 0; i < names.length; i++){
+        let newArray = []
+        let message = `Thank you, ${names[i]} , for the wonderful ${randomName} gift!`
+        newArray.push(message)
+        return newArray
+    }
+}
+writeCards(["Guadalupe", "Ollie", "Aki"],"surprise")
